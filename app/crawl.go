0739cd48277eee9d14174cf63c5b3fcceaf0398c
bytemare <{ID}+{username}@users.noreply.github.com>
2019-10-04 20:40:31 +0200

    Added check for arguments

--- app/crawl.go
+++ app/crawl.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/bytemare/crawl"
-	"github.com/sirupsen/logrus"
 	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -12,14 +13,19 @@ func main() {
 
 	// Define and parse command line arguments
 	timeout := flag.Int("timeout", 0, "crawling time, in seconds. 0 or none is infinite.")
-	domain := flag.String("domain", "", "crawling scope / target domain")
-	flag.Parse()
+
+	if len(os.Args) < 2 {
+		fmt.Printf("Expecting at least an url as entry point. e.g. './%s https://bytema.re'\n", filepath.Base(os.Args[0]))
+		os.Exit(1)
+	}
+
+	domain := os.Args[1]
 
 	// Launch crawler
-	err := crawl.Crawl(*domain, time.Duration(*timeout)*time.Second)
+	err := crawl.Crawl(domain, time.Duration(*timeout)*time.Second)
 
 	if err != nil {
-		logrus.Error(err)
+		fmt.Printf("Error : %s\n", err)
 		os.Exit(1)
 	}
 	os.Exit(0)

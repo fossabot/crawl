f7958d97b673e0a27bdbc4607a9d06640fed470c
bytemare <{ID}+{username}@users.noreply.github.com>
2019-10-06 21:34:45 +0200

    added stuff to doc

--- doc.go
+++ doc.go
@@ -1,5 +1,25 @@
 /*
 Package crawl is a simple web crawler with single domain scope. It can be limited with a timeout.
-*/
 
+
+
+
+					controler --
+						|
+						|
+					 crawler --- Manage workers, filter links, relay validated results to controller
+					/  |
+				   /   |
+			  worker  worker --- Visit an url, scrap for links, return Result to crawler
+
+
+
+
+
+
+
+
+
+
+*/
 package crawl

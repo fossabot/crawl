133ffc1bad58338bcb38f03c1ae0912537c9bf27
bytemare <{ID}+{username}@users.noreply.github.com>
2019-10-08 03:52:40 +0200

    updated documentation

--- doc.go
+++ doc.go
@@ -1,25 +1,13 @@
 /*
-Package crawl is a simple web crawler with single domain scope. It can be limited with a timeout.
+Package crawl is a simple web crawler with single domain scope.
+It can be limited with a timeout and interrupted with SIGINT or SIGTERM.
 
+The FetchLinks and StreamLinks functions start at a given url, scrap the content of the corresponding web page for links
+to the same host, and crawl the whole web site through all encountered links.
+The return values can be used for a site map.
 
+Some precautions have been taken to prevent infinite loops, like stripping queries and fragments off urls.
 
-
-					controler --
-						|
-						|
-					 crawler --- Manage workers, filter links, relay validated results to controller
-					/  |
-				   /   |
-			  worker  worker --- Visit an url, scrap for links, return Result to crawler
-
-
-
-
-
-
-
-
-
-
+A sample program calling the package is given in the project repository.
 */
 package crawl
